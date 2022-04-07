ec1f9e10a3dae10d1f769df0b614bcf1540e2e8e
j.mellado.fuentes <juliamelladofuentes@iessoterohernandez.es>
2022-04-07 10:02:52 +0200

    Commit 1

---
+++ src/edu/fpdual/ejemplo/project/Main.java
@@ -0,0 +1,19 @@
+package edu.fpdual.ejemplo.project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.printMessage();
+
+
+    }
+
+    public void printMessage(){
+        int variableInt = 5;
+
+        System.out.println(variableInt);
+    }
+
+
+}
